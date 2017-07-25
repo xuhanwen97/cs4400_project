c254b937143456b78296a2db8ceb827eb3820226
Michael Xu <michael@neadwerx.com>
2017-07-25 13:47:30 +0200

    Added 4th query to search_query_test.sql file

--- search_query_test.sql
+++ search_query_test.sql
@@ -27,6 +27,44 @@ select trip_train_id, trip_id, embark_stop_id, disembark_stop_id, train_id, stop
 \! echo This grabs direct trains that go from one station to another
 select stop.station_id, stop.train_id, stop.stop_time, stop1.station_id as station2_id, stop1.stop_time as stop_time2 from stop join stop as stop1 where stop.station_id = (select station_id from station where name = 'Metz') and stop1.station_id = (select station_id from station where name = 'Aix') and stop.train_id = stop1.train_id;
 
+-- List the two train trips from Metz to Aix including the customer who booked the trip
+select station_id into @metz_station_id from station where name = 'Metz';
+select station_id into @aix_station_id from station where name = 'Aix';
+
+\! echo Grabs all trains that leave from Metz
+select stop.station_id, stop.train_id, stop.stop_time, stop2.station_id as station2_id, stop2.train_id as train2_id, stop2.stop_time as stop2_time from stop join stop as stop2
+    where stop.station_id = @metz_station_id;
+
+
+\! echo Grabs all trains that arrive in Aix
+select stop.station_id, stop.train_id, stop.stop_time, stop2.station_id as station2_id, stop2.train_id as train2_id, stop2.stop_time as stop2_time from stop join stop as stop2
+    where stop2.station_id = @aix_station_id;
+
+\! echo attempt to grab trains that leave from metz and arrive in aix that share the same intermediate stop
+select * from
+    (
+        select stop.station_id, stop.train_id, stop.stop_time, stop2.station_id as station2_id, stop2.train_id as train2_id, stop2.stop_time as stop2_time from stop join stop as stop2
+        where stop.station_id = @metz_station_id
+    ) as metz_depart
+    join
+    (
+        select stop.station_id, stop.train_id, stop.stop_time, stop2.station_id as station2_id, stop2.train_id as train2_id, stop2.stop_time as stop2_time from stop join stop as stop2
+        where stop2.station_id = @aix_station_id
+    ) as aix_arrive
+    -- departs from metz, doesn't go from metz - metz for leg 1, same train id during leg 1, leg 1 ending station = leg 2 beginning station, same train id during leg 2, doesn't go from aix - aix for leg 2, arrives at aix
+    on metz_depart.station_id = @metz_station_id
+        and metz_depart.station_id != metz_depart.station2_id
+        and metz_depart.train_id = metz_depart.train2_id
+        and metz_depart.station2_id = aix_arrive.station_id
+        and aix_arrive.train_id = aix_arrive.train2_id
+        and aix_arrive.station_id != aix_arrive.station2_id
+        and aix_arrive.station2_id = @aix_station_id;
+
+
+
+
+
+
 
 
 
